b04ebadbb57ab2a487ebe314fb0cf32a78e6415e
kingsporty147 <93953667+kingsporty147@users.noreply.github.com>
2021-11-19 01:18:36 +0100

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,65 @@
+import random
+
+
+def start_game():
+
+    gameover = 0
+    counter = 0
+    randNumberStart = 1
+    randNumberEnd = 10
+    answer = random.randint(randNumberStart, randNumberEnd)
+    highScore = 0
+
+    print('Number Guessing Game')
+
+    playerName = raw_input('What is your name?  ')
+
+
+    print("Welcome to Number Guessing Game, {}".format(playerName))
+
+    while gameover == 0:
+        counter += 1
+
+        print("Current high score: {}".format(highScore))
+        magicNumber = raw_input("Guess the magic number from 1-10?  ")
+
+        try:
+            magicNumber = int(magicNumber)
+
+            if magicNumber > randNumberEnd:
+                raise ValueError(
+                    "Please try again with number between {} and {}".format(
+                        randNumberStart, randNumberEnd))
+
+            if magicNumber > answer:
+                raise ValueError("It's lower")
+            if magicNumber < answer:
+                raise ValueError("It's higher")
+
+        except ValueError as err:
+            print("Error: {}".format(err))
+
+        else:
+            if magicNumber == answer:
+                print("Got it, it took you {} attempts ".format(counter))
+                print("Game Over")
+
+                checkIfplayAgain = raw_input("Do you want to play again?  ")
+
+                if checkIfplayAgain == "y":
+
+                    if counter < highScore:
+                        highScore = counter
+                    elif highScore == 0:
+                        highScore = counter
+
+                    gameover = 0
+                    counter = 0
+                    answer = random.randint(randNumberStart, randNumberEnd)
+
+                else:
+                    print("Goodbye")
+                    gameover = 1
+
+
+start_game()
